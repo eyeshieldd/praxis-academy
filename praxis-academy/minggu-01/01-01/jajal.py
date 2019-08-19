80f309184b7fb30aeb403d011740c34e05798c0f
johan18 <johanajisetiawan1118@gmail.com>
2019-08-19 03:56:01 -0400

    pelajaran minggu 1

---
+++ praxis-academy/minggu-01/01-01/jajal.py
@@ -0,0 +1,14 @@
+print('{0:.3f}'.format(1.0/3))
+
+age = 25
+name = 'johan'
+print('umur saya', age )
+
+c ='johan'
+print(c[2:5])
+
+masuk = int(input('jam masuk : '))
+keluar = int(input('jam keluar : '))
+
+
+
